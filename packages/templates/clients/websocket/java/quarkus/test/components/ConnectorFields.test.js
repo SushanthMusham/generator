cdefeee61bfb2d7788df9761126c73ac72e21285
DORAEMONlovesMICHAN <mushamsushanth@gmail.com>
2026-08-17 00:13:29 +0530

    test: add component tests for ConnectorFields

---
+++ packages/templates/clients/websocket/java/quarkus/test/components/ConnectorFields.test.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const React = require('react');
+const { render } = require('@asyncapi/generator-react-sdk');
+
+const { ConnectorFields } = require(path.resolve(__dirname, '..', '..', 'components', 'ConnectorFields.js'));
+
+describe('ConnectorFields component (Quarkus WebSocket)', () => {
+  test('no query params (undefined) - snapshot', () => {
+    const result = render(<ConnectorFields clientName="NotificationsClient" queryParamsArray={undefined} />);
+    expect(result.trim()).toMatchSnapshot();
+  });
+
+  test('no query params (empty array) - snapshot', () => {
+    const result = render(<ConnectorFields clientName="NotificationsClient" queryParamsArray={[]} />);
+    expect(result.trim()).toMatchSnapshot();
+  });
+
+  test('with query params - snapshot', () => {
+    const result = render(<ConnectorFields clientName="NotificationsClient" queryParamsArray={['userId']} />);
+    expect(result.trim()).toMatchSnapshot();
+  });
+
+  test('robustness: empty clientName - snapshot', () => {
+    const result = render(<ConnectorFields clientName="" queryParamsArray={['p']} />);
+    expect(result.trim()).toMatchSnapshot();
+  });
+});
