b19fd5e4f20cc205bebc62256f055d1b5abda609
DORAEMONlovesMICHAN <mushamsushanth@gmail.com>
2026-08-24 19:21:56 +0530

    Used Fixtures

--- packages/templates/clients/websocket/java/quarkus/test/components/ConnectorFields.test.js
+++ packages/templates/clients/websocket/java/quarkus/test/components/ConnectorFields.test.js
@@ -1,9 +1,30 @@
+<<<<<<< Updated upstream
 const path = require('path');
 const { render } = require('@asyncapi/generator-react-sdk');
+=======
+import path from 'path';
+import { render } from '@asyncapi/generator-react-sdk';
+import { Parser, fromFile } from '@asyncapi/parser';
+import { getQueryParams } from '@asyncapi/generator-helpers';
+>>>>>>> Stashed changes
 
-const { ConnectorFields } = require(path.resolve(__dirname, '..', '..', 'components', 'ConnectorFields.js'));
+import { ConnectorFields } from '../../components/ConnectorFields.js';
+
+const parser = new Parser();
+const asyncapiFilePath = path.resolve(__dirname, '../../../../test/__fixtures__/asyncapi-websocket-components.yml');
 
 describe('ConnectorFields component (Quarkus WebSocket)', () => {
+  let queryParamsArray = [];
+
+  beforeAll(async () => {
+    const parseResult = await fromFile(parser, asyncapiFilePath).parse();
+    const parsedAsyncAPIDocument = parseResult.document;
+    const channels = parsedAsyncAPIDocument.channels();
+    const queryParamsMap = getQueryParams(channels);
+
+    queryParamsArray = queryParamsMap ? Array.from(queryParamsMap.entries()) : [];
+  });
+
   test('no query params (undefined) - snapshot', () => {
     const result = render(<ConnectorFields clientName="NotificationsClient" queryParamsArray={undefined} />);
     expect(result.trim()).toMatchSnapshot();
@@ -15,12 +36,12 @@ describe('ConnectorFields component (Quarkus WebSocket)', () => {
   });
 
   test('with query params - snapshot', () => {
-    const result = render(<ConnectorFields clientName="NotificationsClient" queryParamsArray={['userId']} />);
+    const result = render(<ConnectorFields clientName="NotificationsClient" queryParamsArray={queryParamsArray} />);
     expect(result.trim()).toMatchSnapshot();
   });
 
   test('robustness: empty clientName - snapshot', () => {
-    const result = render(<ConnectorFields clientName="" queryParamsArray={['p']} />);
+    const result = render(<ConnectorFields clientName="" queryParamsArray={queryParamsArray} />);
     expect(result.trim()).toMatchSnapshot();
   });
 });
